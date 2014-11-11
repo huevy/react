bb11dc697a61daa4c528b49a6049ac6f65fbeba3
huevy <huyovy@gmail.com>
2014-11-11 17:46:14 +0200

    routing init

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,18 @@
  */
 
 var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var Routes = Router.Routes;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
 
 var App = require('./App.jsx');
 
 React.renderComponent(
-  <App />, document.getElementById('app'));
+  (
+    <Routes location="history">
+      <Route name="index" path="/" handler={App} />
+    </Routes>
+  ), document.getElementById('app'));
